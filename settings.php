07bb0d8f65b11362d9b65f1f4be922415690fb66
Robin Ellul <106514824@student.swin.edu.au>
2026-05-25 22:14:58 +1000

    start on jobs and edited settings.php

--- settings.php
+++ settings.php
@@ -3,4 +3,10 @@ $host = "localhost";
 $user = "root";
 $pwd = "";
 $sql_db = "db_name"; // Replace with your actual DB name
+
+$conn = mysqli_connect($host, $user, $pwd, $sql_db);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 ?>
