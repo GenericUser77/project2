bb6118ce5d9b95253847e9a1dcc25a30edc5329e
Robin Ellul <106514824@student.swin.edu.au>
2026-05-26 15:36:04 +1000

    more testing

--- settings.php
+++ settings.php
@@ -4,7 +4,7 @@ $user = "root";
 $pwd = "";
 $sql_db = "jobs"; 
 
-$conn = mysqli_query($host, $user, $pwd, $sql_db);
+$conn = mysqli_connect($host, $user, $pwd, $sql_db);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_query_error());
