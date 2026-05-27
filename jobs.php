96e1810c561b59941cd43bdf65f49f713f236973
Robin Ellul <106514824@student.swin.edu.au>
2026-05-27 19:07:41 +1000

    actually developing the job site. further changes to jobs.php

--- jobs.php
+++ jobs.php
@@ -13,8 +13,43 @@
 $query = "SELECT * FROM jobs";
 $result = mysqli_query($conn, $query); ?>
 
-<h1>Job Listings</h1>
+<h1>Jobs Descriptions</h1>
+<div class="jobs-container">
+<?php
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
 
+        $skills = explode(",", $row['skills']); /* I asked claude how to split the skills into an array, this was the result of my question. */
+?>
+<section class="job-card">
+
+    <h2><?php echo $row['title']; ?></h2>
+
+    <p><?php echo $row['description']; ?></p>
+
+    <p>Key skills include:</p>
+    <ul>
+        <?php foreach ($skills as $skill): ?>
+            <li><?php echo trim($skill); ?></li>
+        <?php endforeach; ?>
+    </ul>
+
+    <p>
+        <?php echo $row['job_code']; ?> - 
+        Salary: $<?php echo number_format($row['salary_min']); ?> - 
+        $<?php echo number_format($row['salary_max']); ?> per year
+    </p>
+
+</section>
+<?php
+    }
+} else {
+    echo "<p>No jobs found.</p>";
+}
+
+mysqli_close($conn);
+?>
+</div>
 
 <?php
 include 'nav.inc';
