3bbf2137c0f6f80d5d8192b405b8fcafa08929db
Robin Ellul <106514824@student.swin.edu.au>
2026-05-28 12:39:55 +1000

    fixed some symbols in the head of jobs.php

--- jobs.php
+++ jobs.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta author="Robin Ellul Romero">'
+    <meta author="Robin Ellul Romero">
     <meta description="A page to show and describe the jobs of the company.">
-    <title>Jobs</title>'
+    <title>Jobs</title>
     <link rel="stylesheet" href="styles/styles.css">
 </head>
 <body>
