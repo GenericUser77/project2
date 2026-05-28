0ae8fc0ad7bd847b5c6572186a8fd7733eae9584
Elijah <105918797@student.swin.edu.au>
2026-05-28 11:40:16 +1000

    changed most of php to html in jobs.php because

--- jobs.php
+++ jobs.php
@@ -1,28 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta author="Robin Ellul Romero">'
+    <meta description="A page to show and describe the jobs of the company.">
+    <title>Jobs</title>'
+    <link rel="stylesheet" href="styles/styles.css">
+</head>
+<body>
 <?php
-echo '<!DOCTYPE html>';
-echo '<html lang="en">';
-echo '<head>';
-echo '    <meta charset="UTF-8">';
-echo '    <meta name="viewport" content="width=device-width, initial-scale=1.0">';
-echo '    <meta author="Robin Ellul Romero">';
-echo '    <meta description="A page to show and describe the jobs of the company.">';
-echo '    <title>Jobs</title>';
-echo '    <link rel="stylesheet" href="styles/styles.css">';
-echo '</head>';
-echo '<body>';
 require_once("settings.php");
 $query = "SELECT * FROM jobs";
 $result = mysqli_query($conn, $query); 
 include 'header.inc';
-echo '<h2>Jobs Descriptions</h2>';
-echo '<div class="jobs-container" > ';
+?>
+<h2>Jobs Descriptions</h2>
+<div class="jobs-container">
+<?php
 
 if (mysqli_num_rows($result) > 0) {
     while ($row = mysqli_fetch_assoc($result)) {
 
         $skills = explode(",", $row['skills']); /* I asked claude how to split the skills into an array, this was the result of my question. */
-echo '<section class="job-card">';
-
+?>
+        <section class="job-card">
+<?php
 echo '<h2>' . $row['title'] . '</h2>';
 echo '<h3>' . $row['job_code'] . '</h3>';
 echo '<p>' . $row['description'] . '</p>';
